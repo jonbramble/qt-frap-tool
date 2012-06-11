ff9c01dbf782f3509f698a33bad7da28cd0b2a33
Jonathan Bramble <j.p.bramble@leeds.ac.uk>
2012-06-11 17:49:46 +0100

    added label

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,7 +12,7 @@ MainWindow::MainWindow(QWidget *parent) :
     connect(ui->actionImage_Stack, SIGNAL(triggered()), this, SLOT(imagelist_file_open()));
     connect(ui->actionRun_Experiment, SIGNAL(triggered()), this, SLOT(run_experiment()));
 
-    starting_dir = "//home//jon//Programming//C//frap-lib-test//data";
+    starting_dir = "/home/DS/phyjpb/Programming/C/frap-lib-test/data";
 }
 
 MainWindow::~MainWindow()
@@ -22,21 +22,28 @@ MainWindow::~MainWindow()
 
 void MainWindow::run_experiment()
 {
-    experiment = new FrapTool::Frap(qPrintable(background_file_name),qPrintable(closedapp_file_name),false);
+    if(set_background && set_closed && set_image_list)
+    {
+        experiment = new FrapTool::Frap(qPrintable(background_file_name),qPrintable(closedapp_file_name),false);
 
-    experiment->doselection();
+        experiment->doselection();
 
-    ifiles.reserve(image_string_list.size()); // reserve vector space
-    std::transform(image_string_list.begin(),image_string_list.end(),std::back_inserter(ifiles),&qStringToSTLString);
+        experiment->setimagenames(ifiles);
+        experiment->processdata();
 
-    experiment->setimagenames(ifiles);
-    experiment->processdata();
+        experiment->plplot_chart("prefix");
+        experiment->print_data();
+        //experiment->save_data_file(prefix);
 
-    //experiment->plplot_chart(prefix);
-    experiment->print_data();
-    //experiment->save_data_file(prefix);
+        delete experiment;
+    }
+    else {
+        QMessageBox::warning(this,
+                                 tr("qt-frap-tool"),
+                                 tr("Image files not set.") );
+    }
 
-   delete experiment;
+
 }
 
 std::string MainWindow::qStringToSTLString(const QString& qstring)
@@ -87,6 +94,9 @@ void MainWindow::imagelist_file_open(){
         if (dialog.exec()) {
             image_string_list = dialog.selectedFiles();
             set_image_list = true;
+
+            ifiles.reserve(image_string_list.size()); // reserve vector space
+            std::transform(image_string_list.begin(),image_string_list.end(),std::back_inserter(ifiles),&qStringToSTLString);
         }
         else
         {
